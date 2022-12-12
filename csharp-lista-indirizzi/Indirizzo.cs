7dde833f6f0750b27b04a85ba92b35f1b90fdae0
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-12-12 16:21:10 +0100

    Add project files.

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Indirizzo
+    {
+        
+            private string nome;
+            private string cognome;
+            private string via;
+            private string città;
+            private string provincia;
+            private int zip;
+        public Indirizzo(string nome, string cognome, string via, string città, string provincia, int zip)
+        {
+            this.nome = nome;
+            this.cognome =cognome;
+            this.via = via;
+            this.città = città;
+            this.provincia= provincia;
+            this.zip = zip;
+
+        }
+        public string GetNome() {
+            return nome;
+        }
+        public string GetCognome()
+        {
+            return cognome;
+        }
+        public string GetVia()
+        {
+            return via;
+        }
+        public string GetProvincia()
+        {
+            return provincia;
+        }
+        public string GetCittà()
+        {
+            return città;
+        }
+        public int GetZip()
+        {
+            return zip;
+        }
+        
+    }
+}
